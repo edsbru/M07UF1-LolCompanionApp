624db4d32394acdaa70a2f19c72049eb7af3187c
JoanDiazComes <joan.diaz@enti.cat>
2023-12-27 18:40:23 +0100

    Mejorando problemas de configuracion

--- app/src/main/java/com/enti/dostres/dam/eduardosalazarbrufau/joandiazcomes/lolappcompanion/screens/ConfigScreen.kt
+++ app/src/main/java/com/enti/dostres/dam/eduardosalazarbrufau/joandiazcomes/lolappcompanion/screens/ConfigScreen.kt
@@ -1,9 +1,32 @@
 package com.enti.dostres.dam.eduardosalazarbrufau.joandiazcomes.lolappcompanion.screens
 
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import com.enti.dostres.dam.eduardosalazarbrufau.joandiazcomes.lolappcompanion.R
 
+class ConfigScreen : AppCompatActivity() {
 
-class ConfigScreen: Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.config_screen)
 
+        val tituloConfiguracion = findViewById<TextView>(R.id.tituloConfiguracion)
+        val btnLogout = findViewById<Button>(R.id.btnLogout)
+
+        // Configuración del título
+        tituloConfiguracion.text = "Settings"
+
+        // Configuración del botón de logout
+        btnLogout.setOnClickListener {
+            val intent = Intent(this, LoginScreen::class.java)
+            startActivity(intent)
+
+            /* Cierra la actividad */
+            finish()
+        }
+    }
 }
