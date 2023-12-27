a139be988f3d1cd3ea0d4b9e5ff57aa012a6e700
JoanDiazComes <joan.diaz@enti.cat>
2023-12-27 16:47:18 +0100

    Empezamos con la pantalla de configuracion

--- app/src/main/java/com/enti/dostres/dam/eduardosalazarbrufau/joandiazcomes/lolappcompanion/screens/ConfigScreen.kt
+++ app/src/main/java/com/enti/dostres/dam/eduardosalazarbrufau/joandiazcomes/lolappcompanion/screens/ConfigScreen.kt
@@ -1,9 +1,34 @@
 package com.enti.dostres.dam.eduardosalazarbrufau.joandiazcomes.lolappcompanion.screens
 
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import com.enti.dostres.dam.eduardosalazarbrufau.joandiazcomes.lolappcompanion.R
 
+class ConfiguracionActivity : AppCompatActivity() {
 
-class ConfigScreen: Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.config_screen)
 
+        val tituloConfiguracion = findViewById<TextView>(R.id.tituloConfiguracion)
+        val btnLogout = findViewById<Button>(R.id.btnLogout)
+
+        // Configuración del título
+        tituloConfiguracion.text = "Settings"
+
+        // Configuración del botón de logout
+        btnLogout.setOnClickListener {
+            // Realizar las operaciones de logout aquí
+            // Por ejemplo, redirigir a la pantalla de inicio de sesión
+            val intent = Intent(this, LoginScreen::class.java)
+            startActivity(intent)
+
+            /* Cierra la actividad actual */
+            finish()
+        }
+    }
 }
