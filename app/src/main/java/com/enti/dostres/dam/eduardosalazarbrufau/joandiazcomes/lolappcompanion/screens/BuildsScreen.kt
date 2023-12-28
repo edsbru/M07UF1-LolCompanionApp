cb49d6c805cc41261b534151f63ccae8941aedfc
JoanDiazComes <joan.diaz@enti.cat>
2023-12-28 17:31:09 +0100

    empezamos la pagina de builds

---
+++ app/src/main/java/com/enti/dostres/dam/eduardosalazarbrufau/joandiazcomes/lolappcompanion/screens/BuildsScreen.kt
@@ -0,0 +1,20 @@
+
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.enti.dostres.dam.eduardosalazarbrufau.joandiazcomes.lolappcompanion.R
+
+class BuildsScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.builds_screen)
+
+        val tituloBuilds = findViewById<TextView>(R.id.tituloBuilds)
+        val editTextBuilds = findViewById<EditText>(R.id.editTextBuilds)
+
+        // Configuración del título
+        tituloBuilds.text = "Builds"
+    }
+}
